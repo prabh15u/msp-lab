72028cd04e067ca2f2f22c0378c21fe31fbfe4bb
prabh15u <54323967+prabh15u@users.noreply.github.com>
2026-09-15 00:29:40 -0600

    Fix formatting in variables.tf for admin_username

--- environments/az-305/variables.tf
+++ environments/az-305/variables.tf
@@ -1,10 +1,9 @@
 variable "admin_username" {
-    description = "Admin Username for the Linux VMs"
-    type        = string 
+  description = "Admin Username for the Linux VMs"
+  type        = string
 }
 
 variable "admin_public_key" {
   description = "Admin ssh public key for the Linux VMs"
   type        = string
 }
-
